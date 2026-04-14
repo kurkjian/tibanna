d6d10fb7bfb16213e09a83168a01a1a02282d1db
Joey Kurkjian <kurkjianj@gmail.com>
2026-04-13 23:03:42 -0500

    Write basic lexer
    Currently only supports single line programs in the form:
      `exit(<int>);`
    
    Lexer not actually in use yet as of this commit

---
+++ src/lexer.rs
@@ -0,0 +1,109 @@
+use anyhow::Result;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token {
+    Exit,
+    OpenParen,
+    CloseParen,
+    Int(usize),
+    Semi,
+}
+
+struct Lexer<'a> {
+    text: &'a str,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(text: &'a str) -> Self {
+        Self { text }
+    }
+
+    pub fn tokenize(&mut self) -> Result<Vec<Token>> {
+        let mut tokens = Vec::new();
+        let mut str_buffer = String::new();
+        let mut iter = self.text.chars().peekable();
+
+        while let Some(char) = iter.peek() {
+            if char.is_alphabetic() {
+                str_buffer.push(iter.next().unwrap());
+
+                while let Some(next) = iter.peek()
+                    && next.is_alphanumeric()
+                {
+                    str_buffer.push(iter.next().unwrap());
+                }
+
+                match str_buffer.as_str() {
+                    "exit" => tokens.push(Token::Exit),
+                    _ => return Err(anyhow::anyhow!("Unknown token: {}", str_buffer)),
+                }
+            } else if char.is_ascii_digit() {
+                str_buffer.push(iter.next().unwrap());
+
+                while let Some(next) = iter.peek()
+                    && next.is_ascii_digit()
+                {
+                    str_buffer.push(iter.next().unwrap());
+                }
+
+                tokens.push(Token::Int(str_buffer.parse::<usize>()?));
+            } else if *char == '(' {
+                tokens.push(Token::OpenParen);
+                iter.next();
+            } else if *char == ')' {
+                tokens.push(Token::CloseParen);
+                iter.next();
+            } else if *char == ';' {
+                tokens.push(Token::Semi);
+                iter.next();
+            } else {
+                return Err(anyhow::anyhow!("Unknown token: {}", char));
+            }
+
+            str_buffer.clear();
+        }
+
+        Ok(tokens)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_exit() {
+        let input = "exit";
+        let mut lexer = Lexer::new(input);
+        let tokens = lexer.tokenize().unwrap();
+
+        assert_eq!(tokens, vec![Token::Exit]);
+    }
+
+    #[test]
+    fn test_int() {
+        let input = "123";
+        let mut lexer = Lexer::new(input);
+        let tokens = lexer.tokenize().unwrap();
+
+        assert_eq!(tokens, vec![Token::Int(123)]);
+    }
+
+    #[test]
+    fn test_exit_statement() {
+        let input = "exit(69);";
+        let mut lexer = Lexer::new(input);
+        let tokens = lexer.tokenize().unwrap();
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token::Exit,
+                Token::OpenParen,
+                Token::Int(69),
+                Token::CloseParen,
+                Token::Semi
+            ]
+        );
+    }
+}
