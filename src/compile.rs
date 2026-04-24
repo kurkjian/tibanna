1db7972ee58a1a83934ae0c28278f3c304d8f948
Joey Kurkjian <kurkjianj@gmail.com>
2026-04-23 23:11:07 -0500

    Fix local variable offset bugs
    The crux of the problem is that im not actually reserving stack space
    for local variables; im just yoloing them onto the stack via `push` and
    hoping to index them correctly after that.
    
    The previous commit attempted to fix by indexing off of rbp instead of
    rsp since rbp would be fixed for the stack frame, but that is only a
    purely correct solution for the first variable - there can still be
    intermediate push and pops for inefficient codegen.
    
    So this commit bandaids that by incrementing and decrementing the stack
    offset for every push and pop generated. It's hacky and stupid, and the
    real way i should fix it later is by actually reserving stack space and
    moving to that location for local vars.
    
    Something like:
    ```
    ...preamble
    sub rsp, 16 ; reserve 16 bytes for 2 8-byte variables
    
    mov rax, 6
    sub rax, 2
    mov [rbp-8], rax ; store 'x'
    
    ...calculate and store 'y' @ [rbp-16]
    ```

--- src/compile.rs
+++ src/compile.rs
@@ -6,6 +6,7 @@ use crate::{
 };
 
 const EXIT_SYSCALL: usize = 60;
+const WORD_SIZE: usize = 8;
 
 #[derive(Debug, Clone, Copy)]
 enum VariableLocation {
@@ -19,7 +20,9 @@ pub struct Compiler {
 
 impl Compiler {
     pub fn new() -> Self {
-        Self { stack_offset: 8 }
+        Self {
+            stack_offset: WORD_SIZE,
+        }
     }
 
     pub fn compile(&mut self, program: Program) -> Vec<Instruction> {
@@ -28,11 +31,8 @@ impl Compiler {
 
         for stmt in program.statements {
             self.compile_statement(stmt, &mut instructions, &mut identifiers);
-
-            println!("DEBUG: {instructions:?}")
         }
 
-        println!("DEBUG: {identifiers:?}");
         instructions
     }
 
@@ -70,6 +70,8 @@ impl Compiler {
                         self.compile_expr(*rhs, instructions, identifiers);
                         instructions.push(Instruction::Pop(Reg::Rdi));
                         instructions.push(Instruction::Pop(Reg::Rax));
+                        self.stack_offset -= 2 * WORD_SIZE;
+
                         instructions.push(Instruction::Add(Reg::Rdi, Reg::Rax));
                     }
                     ExpressionVariant::BinarySub(lhs, rhs) => {
@@ -77,6 +79,8 @@ impl Compiler {
                         self.compile_expr(*rhs, instructions, identifiers);
                         instructions.push(Instruction::Pop(Reg::Rdi));
                         instructions.push(Instruction::Pop(Reg::Rax));
+                        self.stack_offset -= 2 * WORD_SIZE;
+
                         instructions.push(Instruction::Sub(Reg::Rdi, Reg::Rax));
                     }
                 }
@@ -102,13 +106,10 @@ impl Compiler {
                             offset,
                         };
                         identifiers.insert(ident.name, VariableLocation::Offset(self.stack_offset));
-                        self.stack_offset += 8;
-
                         Arg64::Mem(mem_ref)
                     }
                     ExpressionVariant::IntLit(value) => {
                         let loc = VariableLocation::Offset(self.stack_offset);
-                        self.stack_offset += 8;
                         identifiers.insert(ident.name, loc);
 
                         Arg64::Unsigned(value)
@@ -118,10 +119,11 @@ impl Compiler {
                         self.compile_expr(*rhs, instructions, identifiers);
                         instructions.push(Instruction::Pop(Reg::Rax));
                         instructions.push(Instruction::Pop(Reg::Rbx));
+                        self.stack_offset -= 2 * WORD_SIZE;
+
                         instructions.push(Instruction::Add(Reg::Rax, Reg::Rbx));
 
                         let loc = VariableLocation::Offset(self.stack_offset);
-                        self.stack_offset += 8;
                         identifiers.insert(ident.name, loc);
 
                         Arg64::Reg(Reg::Rax)
@@ -131,10 +133,11 @@ impl Compiler {
                         self.compile_expr(*rhs, instructions, identifiers);
                         instructions.push(Instruction::Pop(Reg::Rbx));
                         instructions.push(Instruction::Pop(Reg::Rax));
+                        self.stack_offset -= 2 * WORD_SIZE;
+
                         instructions.push(Instruction::Sub(Reg::Rax, Reg::Rbx));
 
                         let loc = VariableLocation::Offset(self.stack_offset);
-                        self.stack_offset += 8;
                         identifiers.insert(ident.name, loc);
 
                         Arg64::Reg(Reg::Rax)
@@ -145,6 +148,7 @@ impl Compiler {
                 // This can be optimized later to avoid the stack probably (register allocation, code-gen ??)
                 instructions.push(Instruction::Mov(Mov::ToReg(Reg::Rax, arg)));
                 instructions.push(Instruction::Push(Reg::Rax));
+                self.stack_offset += WORD_SIZE;
             }
         }
     }
@@ -162,22 +166,29 @@ impl Compiler {
                     Arg64::Unsigned(value),
                 )));
                 instructions.push(Instruction::Push(Reg::Rax));
+                self.stack_offset += WORD_SIZE;
             }
             ExpressionVariant::BinaryAdd(lhs, rhs) => {
                 self.compile_expr(*lhs, instructions, identifiers);
                 self.compile_expr(*rhs, instructions, identifiers);
                 instructions.push(Instruction::Pop(Reg::Rax));
                 instructions.push(Instruction::Pop(Reg::Rbx));
+                self.stack_offset -= 2 * WORD_SIZE;
+
                 instructions.push(Instruction::Add(Reg::Rax, Reg::Rbx));
                 instructions.push(Instruction::Push(Reg::Rax));
+                self.stack_offset += WORD_SIZE;
             }
             ExpressionVariant::BinarySub(lhs, rhs) => {
                 self.compile_expr(*lhs, instructions, identifiers);
                 self.compile_expr(*rhs, instructions, identifiers);
                 instructions.push(Instruction::Pop(Reg::Rax));
                 instructions.push(Instruction::Pop(Reg::Rbx));
+                self.stack_offset -= 2 * WORD_SIZE;
+
                 instructions.push(Instruction::Sub(Reg::Rax, Reg::Rbx));
                 instructions.push(Instruction::Push(Reg::Rax));
+                self.stack_offset += WORD_SIZE;
             }
             ExpressionVariant::Identifier(name) => {
                 let var = identifiers
@@ -195,6 +206,7 @@ impl Compiler {
                     }),
                 )));
                 instructions.push(Instruction::Push(Reg::Rax));
+                self.stack_offset += WORD_SIZE;
             }
         }
     }
